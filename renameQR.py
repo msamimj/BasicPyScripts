334d584584b05f788cb366b752a204ecb68b4614
msamimj <msamimj@gmail.com>
2018-04-13 20:05:39 +0530

    renameQR

--- renameQR.py
+++ renameQR.py
@@ -1,9 +1,9 @@
 import os
 
-offset = 150
+offset = 250
 
 for filename in os.listdir('.'):
     n1 = filename.split(' ')[2].split('.')[0]
-    n2 = int(filename.split(' ')[2].split('.')[0]) + offset
+    n2 = int(n1) + offset
     newfilename = filename.replace(n1, str(n2))
     os.rename(filename, newfilename)
